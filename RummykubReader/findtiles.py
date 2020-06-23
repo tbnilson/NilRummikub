a64639dc63be1ede3bf5a18109f352faa5b60315
tbnilson <tbnilson@gmail.com>
2020-06-23 09:42:03 -0400

    Get list of tile images with correct orientation

--- RummykubReader/findtiles.py
+++ RummykubReader/findtiles.py
@@ -19,15 +19,6 @@ args = vars(ap.parse_args())
 td = TileDetector()
 cap = None
 image = None
-cv2.namedWindow(TRACKBARS)
-cv2.resizeWindow(TRACKBARS, 640, 300)
-cv2.createTrackbar("Hue Min", TRACKBARS, 20, 179, nothing)
-cv2.createTrackbar("Hue Max", TRACKBARS, 69, 179, nothing)
-cv2.createTrackbar("Sat Min", TRACKBARS, 0, 255, nothing)
-cv2.createTrackbar("Sat Max", TRACKBARS, 94, 255, nothing)
-cv2.createTrackbar("Val Min", TRACKBARS, 151, 255, nothing)
-cv2.createTrackbar("Val Max", TRACKBARS, 255, 255, nothing)
-cv2.createTrackbar("threshold", TRACKBARS, 1, 100, nothing)
 
 if args["source"] == "webcam":
     cap = cv2.VideoCapture(3, cv2.CAP_DSHOW)
@@ -41,58 +32,10 @@ while True:
 
     td.set_image(image, resize_width=960)
 
-    h_min = cv2.getTrackbarPos("Hue Min", TRACKBARS)
-    h_max = cv2.getTrackbarPos("Hue Max", TRACKBARS)
-    s_min = cv2.getTrackbarPos("Sat Min", TRACKBARS)
-    s_max = cv2.getTrackbarPos("Sat Max", TRACKBARS)
-    v_min = cv2.getTrackbarPos("Val Min", TRACKBARS)
-    v_max = cv2.getTrackbarPos("Val Max", TRACKBARS)
-    lower = np.array([h_min, s_min, v_min])
-    upper = np.array([h_max, s_max, v_max])
-    img_masked, mask = td.get_masked_img(lower, upper)
+    tile_images = td.get_tile_images()
 
-    # cnts, heirarchy = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    # tiles = td.find_tiles(cnts)
-    #
-    # tilemask = np.zeros(td.image.shape[:2], np.uint8)
-    # cv2.drawContours(tilemask, tiles, -1, 255, -1)
-    # img_masked = cv2.bitwise_and(td.image, td.image, mask=tilemask)
-
-    # cv2.imshow("mask", td.image)
-
-    thresh = td.get_threshold()
-    thresh_cnts, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    thresh_tiles = td.find_tiles(thresh_cnts)
-    thresh_tilemask = np.zeros(td.image.shape[:2], np.uint8)
-    cv2.drawContours(thresh_tilemask, thresh_tiles, -1, 255, -1)
-    img_masked = cv2.bitwise_and(td.image, td.image, mask=thresh_tilemask)
-    cv2.imshow("adaptive threshold", img_masked)
-
-    tile_width = 100
-    tile_height = 150
-    tile_rect = order_points(np.float32([[0, 0], [0, tile_height], [tile_width, tile_height], [tile_width, 0]]))
-    tile_images = []
-    for tile in thresh_tiles:
-        tile32 = np.float32([
-            [tile[0][0][0], tile[0][0][1]],
-            [tile[1][0][0], tile[1][0][1]],
-            [tile[2][0][0], tile[2][0][1]],
-            [tile[3][0][0], tile[3][0][1]],
-        ])
-
-        if tile32[0][0] == 0.0 and tile32[0][1] == 0.0:
-            tile_images.append(np.zeros(tile_rect.shape))
-            break
-        rect = order_points(tile32)
-        print("unorderd")
-        print(tile32)
-        print("ordered")
-        print(rect)
-        matrix = cv2.getPerspectiveTransform(rect, tile_rect)
-        tile_images.append(cv2.perspectiveTransform(td.image, matrix))
-
-    if len(tile_images)>0:
-        cv2.imshow("warped", tile_images[0])
+    for i, tile in enumerate(tile_images):
+        cv2.imshow(str(i), tile)
 
     if args["source"] == "webcam":
         if cv2.waitKey(1) & 0xFF == ord('q'):
@@ -103,4 +46,5 @@ while True:
 
 if args["source"] == "webcam":
     cap.release()
+
 cv2.destroyAllWindows()
--- RummykubReader/rummySearch/tilesearch.py
+++ RummykubReader/rummySearch/tilesearch.py
@@ -1,3 +1,5 @@
+from typing import Any
+
 import cv2
 import imutils
 import numpy as np
@@ -27,16 +29,14 @@ def order_points(points):
             pts = np.delete(pts, minindices[1], axis=0)
     else:
         rect[0] = pts[np.argmin(s)]
-        pts = np.delete(pts,np.argmin(s), axis=0)
+        pts = np.delete(pts, np.argmin(s), axis=0)
 
     s = pts.sum(axis=1)
     maxindices = np.argwhere(s == np.amax(s))
     if len(maxindices) > 1:
-        # print(pts[maxindices[0]][0])
-        # print(pts[maxindices[1]][0])
         if pts[maxindices[0]][0][0] > pts[maxindices[1]][0][0]:
             rect[2] = pts[maxindices[0]]
-            pts = np.delete(pts,maxindices[0], axis=0)
+            pts = np.delete(pts, maxindices[0], axis=0)
         else:
             rect[2] = pts[maxindices[1]]
             pts = np.delete(pts, maxindices[1], axis=0)
@@ -44,14 +44,10 @@ def order_points(points):
         rect[2] = pts[np.argmax(s)]
         pts = np.delete(pts, np.argmax(s), axis=0)
 
-    print("With used points removed: ")
-    print(pts)
     # now, compute the difference between the points, the
     # top-right point will have the smallest difference,
     # whereas the bottom-left will have the largest difference
     diff = np.diff(pts, axis=1)
-    print("diff on axis=1")
-    print(diff)
     rect[1] = pts[np.argmin(diff)]
     rect[3] = pts[np.argmax(diff)]
     # return the ordered coordinates
@@ -90,22 +86,27 @@ class TileDetector:
         self.image = imutils.resize(image, width=resize_width)
         self.resize_ratio = image.shape[0] / float(self.image.shape[0])
 
-    def get_threshold(self, threshold=0, kernel=9):
+    def get_threshold(self, threshold=0, kernel=9, image=None):
         # convert the resized image to grayscale, blur it slightly,
         # and threshold it
-        gray = cv2.cvtColor(self.image, cv2.COLOR_BGR2GRAY)
+        if image is None:
+            image = self.image
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
         blurred = cv2.GaussianBlur(gray, (kernel, kernel), 0)
         # thresh = cv2.adaptiveThreshold(blurred, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, kernel, threshold)
         _, thresh = cv2.threshold(blurred, threshold, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
         return thresh
 
-    def get_masked_img(self, lower, upper):
-        img_hsv = cv2.cvtColor(self.image, cv2.COLOR_BGR2HSV)
-        mask = cv2.inRange(img_hsv, lower, upper)
-        result = cv2.bitwise_and(self.image, self.image, mask=mask)
-        return result, mask
+    def get_tile_contours(self) -> Any:
+        thresh = self.get_threshold()
+        thresh_cnts, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        thresh_tiles = self.filter_tile_contours(thresh_cnts)
+        thresh_tilemask = np.zeros(self.image.shape[:2], np.uint8)
+        cv2.drawContours(thresh_tilemask, thresh_tiles, -1, 255, -1)
+        img_masked = cv2.bitwise_and(self.image, self.image, mask=thresh_tilemask)
+        return thresh_tiles, img_masked
 
-    def find_tiles(self, cnts):
+    def filter_tile_contours(self, cnts):
         tiles = []
         img_area_frac = self.image.shape[0] * self.image.shape[1] * .005
 
@@ -117,3 +118,54 @@ class TileDetector:
                 tiles.append(poly)
 
         return tiles
+
+    def get_tile_images(self, width=200, height=300):
+        thresh_tiles, _ = self.get_tile_contours()
+        tile_rect = np.float32([[0, 0],
+                                [width, 0],
+                                [width, height],
+                                [0, height]])
+        tile_images = []
+        for tile in thresh_tiles:
+            tile_zeroangle = np.float32([
+                [tile[0][0][0], tile[0][0][1]],
+                [tile[1][0][0], tile[1][0][1]],
+                [tile[2][0][0], tile[2][0][1]],
+                [tile[3][0][0], tile[3][0][1]],
+            ])
+
+            if tile_zeroangle[0][0] == 0.0 and tile_zeroangle[0][1] == 0.0:
+                break
+            rect = order_points(tile_zeroangle)
+            rect_rotated = np.roll(rect, 1, axis=0)
+
+            matrix = cv2.getPerspectiveTransform(rect, tile_rect)
+            tile_image_zerorotation = cv2.warpPerspective(self.image, matrix, (width, height))
+
+            matrixr = cv2.getPerspectiveTransform(rect_rotated, tile_rect)
+            tile_image_ninetyrotation = cv2.warpPerspective(self.image, matrixr, (width, height))
+
+            gray_zero = cv2.cvtColor(tile_image_zerorotation, cv2.COLOR_BGR2GRAY)
+            gray_ninety = cv2.cvtColor(tile_image_ninetyrotation, cv2.COLOR_BGR2GRAY)
+
+            intensity_zero = self.get_threshold(image=tile_image_zerorotation)
+            intensity_ninety = self.get_threshold(image=tile_image_ninetyrotation)
+
+            print("First dim of gray image", len(gray_zero))
+
+            zero_sum_high = np.sum(intensity_zero[:len(intensity_zero) // 2], dtype="int32")
+            zero_sum_low = np.sum(intensity_zero[len(intensity_zero) // 2:], dtype="int32")
+
+            ninety_sum_high = np.sum(intensity_ninety[:len(intensity_ninety) // 2], dtype="int32")
+            ninety_sum_low = np.sum(intensity_ninety[len(intensity_ninety) // 2:], dtype="int32")
+            print(ninety_sum_high - ninety_sum_low)
+            print(zero_sum_high - zero_sum_low)
+            if abs(zero_sum_high - zero_sum_low) > abs(ninety_sum_high - ninety_sum_low):
+                tile_images.append(
+                    intensity_zero if zero_sum_high < zero_sum_low else cv2.rotate(intensity_zero,
+                                                                                   cv2.ROTATE_180))
+            else:
+                tile_images.append(
+                    intensity_ninety if ninety_sum_high < ninety_sum_low else cv2.rotate(
+                        intensity_ninety, cv2.ROTATE_180))
+        return tile_images
